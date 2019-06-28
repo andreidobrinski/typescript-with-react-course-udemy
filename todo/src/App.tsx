d1753ca87ced224d49c4ec4debecd608083a34f0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-28 16:43:48 -0400

    add todo form

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+type FormElem = React.FormEvent<HTMLFormElement>;
 
 const App: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    setValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
