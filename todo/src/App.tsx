ec7a5337b5dc92c6981d1b616f6721f50e40412f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-28 16:55:12 -0400

    add ability to complete todos

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -2,12 +2,30 @@ import React, { useState } from 'react';
 
 type FormElem = React.FormEvent<HTMLFormElement>;
 
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
 const App: React.FC = () => {
   const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const handleSubmit = (e: FormElem): void => {
     e.preventDefault();
+    addTodo(value);
     setValue('');
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
   };
 
   return (
@@ -22,6 +40,16 @@ const App: React.FC = () => {
         />
         <button type="submit">Add Todo</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <div key={index}>
+            <div>{todo.text}</div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {todo.complete ? 'Incomplete' : 'Complete'}
+            </button>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
