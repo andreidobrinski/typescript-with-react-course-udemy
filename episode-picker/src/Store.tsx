8387dd56753d8e449bca2f8ce100fdf8793ec121
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-07-02 16:46:11 -0400

    add reducer

--- episode-picker/src/Store.tsx
+++ episode-picker/src/Store.tsx
@@ -5,6 +5,11 @@ interface IState {
   favourites: [];
 }
 
+interface IAction {
+  type: string;
+  payload: any;
+}
+
 const initialState: IState = {
   episodes: [],
   favourites: [],
@@ -12,7 +17,14 @@ const initialState: IState = {
 
 export const Store = React.createContext<IState>(initialState);
 
-function reducer() {}
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function StoreProvider(props: any): JSX.Element {
   return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
