93d71d4a79005eb80d822e2c3c6fa5552e80bdaa
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-07-15 14:47:13 -0400

    add fetch call for episodes

--- episode-picker/src/App.tsx
+++ episode-picker/src/App.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import { Store } from './Store';
 
-const App = (): JSX.Element => {
-  const store = React.useContext(Store);
+interface IEpisode {
+  airdate: string,
+  airstamp: string,
+  airtime: string,
+  id: number,
+  image: {medium: string, original: string},
+  name: string,
+  number: number,
+  runtime: number,
+  season: number,
+  summary: string,
+  url: string
+}
 
-  return <div>app</div>;
+const App = (): JSX.Element => {
+  const {state, dispatch} = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favourite episode</p>
+      <section>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id}>
+              <img src={episode.image && episode.image.medium} alt={`Rick and Morty ${episode.name}`} />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default App;
